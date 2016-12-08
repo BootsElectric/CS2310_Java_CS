dc0a6a08e8d00ab1475c74333b4ce17f9868e2dd
Will <wll.hrdwk@gmail.com>
2016-12-08 23:34:11 +0000

    I've done it YAY!
    Added the ability to search through the indexed texts and print the extended context of the word

--- CS2310 Java CourseWork/src/bookView/BookIndexer.java
+++ CS2310 Java CourseWork/src/bookView/BookIndexer.java
@@ -1,6 +1,7 @@
 package bookView;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -9,49 +10,199 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 
 public class BookIndexer {
-	private Hashtable<String, String> emmaWordIndex;
+	/**
+	 * Hashtables pairing words with WordCoordinates.
+	 **/
+	private Hashtable<String, ArrayList<WordCoordinate>> emmaWordIndex;
+	private Hashtable<String, ArrayList<WordCoordinate>> pandPWordIndex;
+	private Hashtable<String, ArrayList<WordCoordinate>> mansfieldParkWordIndex;
 	
+	private Hashtable<Integer, String> emmaIDIndex;
+	private Hashtable<Integer, String> pandpIDIndex;
+	private Hashtable<Integer, String> mansfieldParkIDIndex;
+	
+	/**
+	 * The current line the BufferReader of index();
+	 * <see>BookIndexer.index()</see> 
+	 **/
+	private String line = "";
+	
+	/**
+	 * Files holding the books so that relative path can be used. 
+	 **/
+	private File emma = new File("data\\emmaEd11.txt");
+	private File pandp = new File("data\\pandpEd12.txt");
+	private File mansfieldPark = new File("data\\mansfieldParkEd10.txt");
+	
+	/**
+	 *Constructor initialises the word indexes to be the indexed files.
+	 **/
 	public BookIndexer(){
-		emmaWordIndex = new Hashtable<>();
+		try{
+		Hashtable[] emmaHashtables = index(emma);
+		Hashtable[] pandpHashtables = index(pandp);
+		Hashtable[] mansfieldParkHashtables = index(mansfieldPark);
+		
+		emmaWordIndex = emmaHashtables[0];
+		pandPWordIndex = pandpHashtables[0];
+		mansfieldParkWordIndex = mansfieldParkHashtables[0];
+		
+		emmaIDIndex = emmaHashtables[1];
+		pandpIDIndex = pandpHashtables[1];
+		mansfieldParkIDIndex = mansfieldParkHashtables[1];
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
 	}
-	
-	public Hashtable<String, String> getEmmaIndex(){
+
+	public Hashtable<String, ArrayList<WordCoordinate>> getEmmaIndex(){
 		return emmaWordIndex;
 	}
 	
-	public void indexEmma(){
-		String path = "C:\\Users\\Will\\Documents\\GitHub\\CS2310 Java CourseWork\\data\\emmaEd11.txt";
+	public Hashtable<String, ArrayList<WordCoordinate>> getPandPIndex(){
+		return pandPWordIndex;
+	}
+	
+	public Hashtable<String, ArrayList<WordCoordinate>> getMansfieldParkIndex(){
+		return mansfieldParkWordIndex;
+	}
+	
+	public Hashtable<Integer, String> getEmmaIDIndex(){
+		return emmaIDIndex;
+	}
+	
+	public Hashtable<Integer, String> getPandPIDIndex(){
+		return pandpIDIndex;
+	}
+	
+	public Hashtable<Integer, String> getMansfiledParkIDIndex(){
+		return mansfieldParkIDIndex;
+	}
+
+	public Hashtable[] index(File file){
+		
+		Hashtable[] hashtables = new Hashtable[2];
+		Hashtable<String, ArrayList<WordCoordinate>> wordIndex = new Hashtable<>();
+		Hashtable<Integer, String> IDIndex = new Hashtable<>();
+		hashtables[0] = wordIndex;
+		hashtables[1] = IDIndex;
+		
 		try {
-			BufferedReader br = new BufferedReader(new FileReader(path));
-			ArrayList<String> lines = new ArrayList<>();
-			
-			while((br.readLine()) != null){
-				lines.add(br.readLine());
-			}
-			
-			ArrayList<String> words = new ArrayList<>();
+			BufferedReader br = new BufferedReader(new FileReader(file.getAbsolutePath()));
 			int lineNumber = 0;
-			int wordNumber = 0;
-			
-			while (lineNumber < lines.size()){
+			int chapterNumber = 0;
+			int paragraphNumber = 1;
+			int volumeNumber = 0;
+			int wordID = 0;
+			while((line = br.readLine()) != null){
 				
-				String[] currentLineWords = lines.get(lineNumber).split(" ");
+				int wordNumber = 0;
+
+				String[] currentLineWords = line.split(" ");
 				while (wordNumber < currentLineWords.length){
-//					words.add(currentLineWords[wordNumber]);
-					emmaWordIndex.put( lineNumber + "," + wordNumber, currentLineWords[wordNumber]);
+					if(!wordIndex.containsKey(currentLineWords[wordNumber])){
+						wordIndex.put(currentLineWords[wordNumber], new ArrayList<WordCoordinate> ());
+					}
+					
+					if (currentLineWords[wordNumber].equals("")){
+						paragraphNumber++;
+						
+					}
+					if (currentLineWords[wordNumber].equals("CHAPTER")){
+						chapterNumber++;
+						paragraphNumber = 1;
+					}
+					if (currentLineWords[wordNumber].equals("VOLUME")){
+						volumeNumber++;
+					}
+
+					wordIndex.get(currentLineWords[wordNumber]).add(
+							new WordCoordinate(wordID, wordNumber, lineNumber, paragraphNumber, chapterNumber, volumeNumber, file));
+					IDIndex.put(wordID, currentLineWords[wordNumber]);
 					wordNumber++;
+					wordID++;
 				}
 				lineNumber++;
 			}
-			
-			/*for(String word : words){
-				emmaWordIndex.put(word, wordPosition[lineNumber][wordNumber]);
-			}*/
-			
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+		return hashtables;
+	}
+	
+	public class WordCoordinate{
+		int ID;
+		int lineNumber;
+		int wordNumber;
+		int paragraphNumber;
+		int chapter;
+		int volume;
+		File title;
+		
+		public WordCoordinate(int ID, int wNum, int lNum, int pNum, int chapter, int volume, File title){
+			this.ID = ID;
+			wordNumber = wNum;
+			lineNumber = lNum;
+			paragraphNumber = pNum;
+			this.chapter = chapter;
+			this.volume = volume;
+			this.title = title;
+		}
+		
+		public int getID(){
+			return ID;
+		}
+		
+		public int getWordNumber(){
+			return wordNumber;
+		}
+		
+		public int getLineNumber(){
+			return lineNumber;
+		}
+		
+		public int getParagraphNumber(){
+			return paragraphNumber;
+		}
+		
+		public int getChapter(){
+			return chapter;
+		}
+		
+		public int getVolume(){
+			return volume;
+		}
+		
+		public String getTitle(){
+			return title.getName();
+		}
+		
+		public String toString(){
+			StringBuffer sb = new StringBuffer();
+			
+			sb.append("Title: ");
+			sb.append(getTitle());
+			if(volume > 0){
+			sb.append("\nVolume: ");
+			sb.append(getVolume());
+			}
+			sb.append("\nChapter: ");
+			sb.append(getChapter());
+			sb.append("\nParagraph: ");
+			sb.append(getParagraphNumber());
+			sb.append("\nLine: ");
+			sb.append(getLineNumber());
+			sb.append(" at: ");
+			sb.append(getWordNumber());
+			sb.append("\nWord ID: ");
+			sb.append(getID());
+			sb.append("\n");
+			
+			return sb.toString();
 		}
 	}
 }
--- CS2310 Java CourseWork/src/bookView/Main.java
+++ CS2310 Java CourseWork/src/bookView/Main.java
@@ -1,7 +1,10 @@
 package bookView;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Hashtable;
+
+import bookView.BookIndexer.WordCoordinate;
 
 public class Main {
 	
@@ -12,14 +15,33 @@ public class Main {
 		long endTime = (new Date()).getTime();
 		System.out.println(endTime - startTime);
 		
+		
 		BookIndexer bi = new BookIndexer();
-		bi.indexEmma();
-		Hashtable<String, String> ht = bi.getEmmaIndex();
-		int i = 0;
-//		while (i < ht.size()){
-			if(ht.containsKey("the")){
-				System.out.println(ht.get(key));
-//			}
-		}
+		Hashtable<String, ArrayList<WordCoordinate>> emmaHt = bi.getEmmaIndex();
+		Hashtable<String, ArrayList<WordCoordinate>> pandpHt = bi.getPandPIndex();
+		Hashtable<String, ArrayList<WordCoordinate>> mansfieldParkHt = bi.getMansfieldParkIndex();
+		
+//		if(emmaHt.containsKey("the")){
+			ArrayList<WordCoordinate> wordCoords = emmaHt.get("the");
+			for(WordCoordinate wordCoord : wordCoords){
+				System.out.println(wordCoord);
+			}
+			System.out.println(wordCoords.size());
+//		}
+//		if(pandpHt.containsKey("the")){
+			ArrayList<WordCoordinate> wordCoords1 = pandpHt.get("the");
+			for(WordCoordinate wordCoord : wordCoords1){
+				System.out.println(wordCoord);
+			}
+			System.out.println(wordCoords1.size());
+//		}
+//		if(mansfieldParkHt.containsKey("the")){
+			ArrayList<WordCoordinate> wordCoords2 = mansfieldParkHt.get("the");
+			for(WordCoordinate wordCoord : wordCoords2){
+				System.out.println(wordCoord);
+			}
+			System.out.println(wordCoords2.size());
+//		}
+		
 	}
 }
